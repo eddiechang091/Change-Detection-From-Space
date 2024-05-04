d022814c5b7f6b33d1631e92f0a70903a1596fe0
Eddie Chang <eddie.chang@lichr.com>
2024-05-04 00:25:17 -0300

    feat(); add empty input folder check

--- challenge1/executable.py
+++ challenge1/executable.py
@@ -7,7 +7,10 @@ output_dir = '../output_queue'
 
 # List all files in the input directory
 files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
-
+# If no files in inupt directory, exit
+if len(files) == 0:
+    print("No files to process.")
+    exit()
 # Move each file to the output directory
 for file in files:
     shutil.move(os.path.join(input_dir, file), os.path.join(output_dir, file))
